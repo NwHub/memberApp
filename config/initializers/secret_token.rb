7b91a7c3d454bb065d69f6125af85f8672ac5ad6
zawa <zawa@zawa-UX31A.(none)>
2012-09-09 01:59:13 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemApp::Application.config.secret_token = '0787be592acdbc7fa94e7cc91dc810ec59e1569ff7c02ebbecae4bbdebd0231cac0158716e5c2945ea8f5d3ed7549890c7b4430836d7008b5df6d1055a5b9c71'
